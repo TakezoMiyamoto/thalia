b1b2c1feba6a5d1a6844fa806c0a5bbe654f296f
takezo <takezo.m.1987@gmail.com>
2018-09-19 09:18:54 +0900

    #5 can user edit and show

--- Gemfile
+++ Gemfile
@@ -45,6 +45,9 @@ gem 'jquery-rails'
 gem 'bootstrap-sass'
 gem 'twitter-bootstrap-rails', '4.0.0'
 
+# form
+gem 'simple_form'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate_user!, only:[:create, :edit, :update, :destroy]
+  before_action :set_user, only:[:show, :edit, :update]
+  def index
+    @user = User.all
+  end
+
+  def show
+    @userName = @user.username
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.errors[:base].empty? and @user.update(edit_user_params)
+      sign_in(@user, :bypass => true)
+      flash[:success] = "プロフィールは更新されました"
+      redirect_to @user
+    else
+      flash[:danger] = "プロフィールの更新に失敗しました"
+      redirect_to user_path(current_user.id)
+    end
+  end
+
+  private
+
+  def edit_user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :avatar)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
