0ed806cd91f895c9658639d2ddd62f53b49c0ddf
takezo <takezo.m.1987@gmail.com>
2018-09-20 08:52:26 +0900

    #6 add introduce colmn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_18_070829) do
+ActiveRecord::Schema.define(version: 2018_09_19_235122) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_09_18_070829) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "avatar"
+    t.text "introduce"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
