18620d14ad71feb18a199d56c6abf67b5dc6b6a5
takezo <takezo.m.1987@gmail.com>
2018-09-20 12:44:26 +0900

    #11 work helper to show youtube video that user posted

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -1,2 +1,8 @@
 module WorksHelper
+  def embed(youtube_url)
+    if @work.youtube_url.present?
+        youtube_id = youtube_url.split("=").last
+        content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+    end
+  end
 end
