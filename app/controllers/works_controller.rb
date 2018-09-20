3b6a5fa0eb22422f377b047774937b962ad9bce3
takezo <takezo.m.1987@gmail.com>
2018-09-20 11:03:11 +0900

    #10 edit works controller actions

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,3 +1,64 @@
 class WorksController < ApplicationController
-  belongs_to :user
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  require 'date'
+
+  def index
+    @works = Work.order(created_at: desc)
+    @title = 'ワークス一覧'
+  end
+
+  def show
+    @work = Work.find(params[:id])
+  end
+
+  def new
+    @work = Work.new
+    @title = '新規ワークの作成'
+  end
+
+  def create
+    @work = current_user.works.build(works_params)
+    if @work.save
+      flash[:success] = "新規Workを作成しました！"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    @work = Work.find(params[:id])
+    if @work.update(works_params)
+      flash[:success] = 'ワーク内容が更新されました。'
+      redirect_to @work
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @work = current_user.works.find_by(id: params[:id])
+    return redirect_to root_url if @work.nil?
+    @work.destroy
+    flash[:success] = 'ワークは削除されました.'
+    redirect_to works_path
+  end
+
+  private
+
+  def works_params
+    params.require(:works).permit(:youtube_id)
+  end
+
+  def works_owner
+    @work = Work.find(params[:id])
+    unless @work.user_id == current_user.id
+      flash[:notice] = 'Access denied as you are not owner of this work'
+      redirect_to works_path
+    end
+  end
 end
