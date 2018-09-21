0de1e1ec87eb1b9c8529c8a8428dc8d493bb0a87
takezo <takezo.m.1987@gmail.com>
2018-09-21 15:58:16 +0900

    #12 edit works_helper.erb and _list.html.erb

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -5,4 +5,12 @@ module WorksHelper
         content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
     end
   end
+
+def imaged(youtube_url)
+  youtube_id = youtube_url.split("=").last
+  image_tag("//img.youtube.com/vi/#{youtube_id}/mqdefault.jpg")
+end
+
+
+
 end
