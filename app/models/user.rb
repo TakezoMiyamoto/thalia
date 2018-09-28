01b310264c4e4bf3c38460c805276ee5eb33d14c
takezo <takezo.m.1987@gmail.com>
2018-09-28 23:07:34 +0900

    #15 modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable, :trackable, :omniauthable, omniauth_providers: %i(google)
 
   has_many :works
   mount_uploader :avatar, AvatarUploader
+
+  protected
+   def self.find_for_google(auth)
+     user = User.find_by(email: auth.info.email)
+
+     unless user
+       user = User.create(username:     auth.info.username,
+                          avatar: auth.avatar,
+                          password: Devise.friendly_token[0, 20],
+                          )
+     end
+     user
+   end
 end
